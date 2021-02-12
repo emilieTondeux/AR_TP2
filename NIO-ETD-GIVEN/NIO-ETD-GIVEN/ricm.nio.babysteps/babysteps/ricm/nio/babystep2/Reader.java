ebbae3ff3ea0120023c630b9d31a055c90dd0ef8
baudeur <baudeurbertrand@yahoo.fr>
2021-02-12 08:17:02 +0100

    baby step2

---
+++ NIO-ETD-GIVEN/NIO-ETD-GIVEN/ricm.nio.babysteps/babysteps/ricm/nio/babystep2/Reader.java
@@ -0,0 +1,15 @@
+package ricm.nio.babystep2;
+
+public class Reader {
+
+	byte[] msg;
+	
+	public void handleRead() {
+		
+	}
+	
+	public void processByte(byte[] msg) {
+		
+	}
+	
+}
---
+++ NIO-ETD-GIVEN/NIO-ETD-GIVEN/ricm.nio.babysteps/babysteps/ricm/nio/babystep2/Writer.java
@@ -0,0 +1,15 @@
+package ricm.nio.babystep2;
+
+public class Writer {
+
+	byte[] msg;
+	
+	public void handleWrite() {
+		
+	}
+	
+	public void sendMsg(byte[] message) {
+		msg = message;
+	}
+	
+}
