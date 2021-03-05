1d74c0b011eee3101bc2e68d85363da372810aa1
baudeur <baudeurbertrand@yahoo.fr>
2021-03-05 09:38:45 +0100

    RealThing

---
+++ NIO-ETD-GIVEN/NIO-ETD-GIVEN/ricm.channels.nio/src/ricm/channels/nio/Broker.java
@@ -0,0 +1,26 @@
+package ricm.channels.nio;
+
+import ricm.channels.IBroker;
+import ricm.channels.IBrokerListener;
+
+public class Broker implements IBroker {
+
+	@Override
+	public void setListener(IBrokerListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean connect(String host, int port) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean accept(int port) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
